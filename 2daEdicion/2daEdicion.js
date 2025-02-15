c83c2c1365519c7c5f21f4d6a7fe4101471fe19d
Franco-Favotti <ffavotti@fi.uba.ar>
2025-02-15 19:05:33 -0300

    Se agregan los datos de la 2da Edicion

---
+++ 2daEdicion/2daEdicion.js
@@ -0,0 +1,413 @@
+function mostrarTitulo(){
+    var titulo = document.getElementById("Titulo");
+    titulo.innerHTML = `
+        <h1>La Liga de los Pataduras</h1>
+    `;
+}
+
+mostrarTitulo();
+
+function mostrarLogo(){
+    var logo = document.getElementById("Logo");
+    logo.innerHTML = `
+        <img width="80px" height="80px" alt="Esto es un logo" src="https://pbs.twimg.com/media/GTcpPQ2XEAEc3x-?format=png&name=small"/>
+    `;
+}
+
+mostrarLogo();
+
+
+function mostrarFixture(){
+    var fixture = document.getElementById("Fixture");
+    fixture.innerHTML = `
+        <header id= "fixtureTitulo">
+            <h2>Fixture</h2>
+        </header>
+        <h3><div class="article">
+            <h3>Fecha 1</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja '>
+                <table border="2">
+                    <tr>
+                        <td><img class= escudoAstrosGambeta width="35px" height="40px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img class= escudoComunidadAnillo width="35px" height="38px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+    
+        </div></h3>
+        
+        <br><br>
+        
+        <h3><div class="article">
+            <h3>Fecha 2</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3> 
+        
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 3</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+
+        </div></h3> 
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 4</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+            
+        </div></h3> 
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 5</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3> 
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 6</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 7</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 8</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 9</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 10</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+        <br><br>
+
+        <h3><div class="article">
+            <h3>Fecha 11</h3>
+            <h4>Serie (0-0)</h4>
+            <div class = 'caja'>
+                <table border="2">
+                    <tr>
+                        <td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td>
+                        <td>Astros de la Gambeta</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>La Comunidad del Anillo</td>
+                        <td><img width="30px" height="30px" alt="La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td>
+
+                    </tr>
+                </table>
+            </div>
+
+            <br>
+
+        </div></h3>
+
+    `;
+}
+
+mostrarFixture();
+
+
+function mostrarGoleadores(){
+    var goleadores = document.getElementById("goleadores");
+    goleadores.innerHTML += `
+        <header id="golesTitulo">
+            <h2>Goleadores</h2>
+        </header>
+        
+        <h3><table border="3">
+            <tr>
+                <th>Equipo</th>
+                <th>Jugador</th>
+                <th>Goles</th>
+    
+            </tr>
+            
+            
+        </table></h3>
+    `;
+}
+
+function mostrarMpvs(){
+    var mvp = document.getElementById("mvp");
+    mvp.innerHTML += `
+        <header id="mvpsTitulo">
+            <h2>MVPS</h2>
+        </header>
+        <h3><table border="3">
+            <tr>
+                <th>Fecha</th>
+                <th>Equipo</th>
+                <th>Jugador</th>
+    
+            </tr>
+           
+        </table></h3>
+    `;
+}
+
+function mostrarTablas(){
+    mostrarGoleadores();
+    mostrarMpvs();
+                    
+}
+
+mostrarTablas();
+
+var equipo1 = ['Franco Favotti (C)', 'Tobias Winjak', 'Manuel Panizo', 'Alejo Osuna', 'Diego Arroyo', 'Facundo Moran', 
+                'Teo Formento', 'Maxi Ventura', 'Nahuel Monti', 'Nahuel Lonero', 'Milton Cabrera'];
+
+var equipo2 = ['Agustin Navarro (C)', 'Tomas Mango', 'Juan Macchi', 'Fabrizio Cecchini', 'Lucas Garcia', 
+                'Jeronimo Hidalgo', 'Enzo Juarez', 'Agustin Lonero', 'Gerardo Vaello', 'Franco Mangiameli'];
+
+function mostrarTituloEquipos(){
+    var  planteles= document.getElementById("planteles");
+    planteles.innerHTML += `
+        <header id="equiposTitulo">
+            <h2>Equipos</h2>
+        </header>
+    `;
+}
+
+function mostrarEquipo1(){
+    var nombre1 = document.getElementById("nombre1");
+    nombre1.innerHTML += `           
+        <h3><table border="0"><tr><td>Astros de la Gambeta</td><td><img width="30px" height="35px" alt="Logo de Astros de la Gambeta" src="https://pbs.twimg.com/media/GjoHU1wXwAANOJr?format=png&name=360x360"/></td></tr></table><h3>
+    `;
+
+    var formacion1 = document.getElementById("formacion1");
+    for(i=0; i<10; i++){
+       
+        formacion1.innerHTML += "<li>"+ equipo1[i]+"</li>";
+         
+    }
+
+    var indumentaria1 = document.getElementById("indumentaria1");
+    indumentaria1.innerHTML += `
+        <h3>
+            Indumentaria: Clara
+        </h3>
+    `;
+}
+
+function mostrarEquipo2(){
+    var nombre2 = document.getElementById("nombre2");
+    nombre2.innerHTML += `           
+        <h3><table border="0"><tr><td>La Comunidad del Anillo</td><td><img width="30px" height="30px" alt="Logo de La Comunidad del Anillo" src="https://pbs.twimg.com/media/Gjq34pIXcAAYx-K?format=png&name=small"/></td></tr></table><h3>
+    `;
+
+    var formacion2 = document.getElementById("formacion2");
+    for(i=0; i<9; i++){
+        formacion2.innerHTML += "<li>"+ equipo2[i]+"</li>";    
+    }
+
+    var indumentaria2 = document.getElementById("indumentaria2");
+
+    indumentaria2.innerHTML += `
+        <h3>
+            Indumentaria: Oscura
+        </h3>
+    `;
+}
+
+function mostrarEquipos(){
+    mostrarTituloEquipos();
+    mostrarEquipo1();
+    mostrarEquipo2();
+
+}
+
+mostrarEquipos();
+
+function mostrarTransferencias(){
+    var transferencias = document.getElementById("transferencias");
+    
+}
+
+mostrarTransferencias();
+
+
+function mostrarFooter(){
+    var footer = document.getElementById("Footer");
+    footer.innerHTML = `
+        Liga de los Pataduras. Todos los derechos reservados.; 
+    `;
+}
+
+mostrarFooter();
+
